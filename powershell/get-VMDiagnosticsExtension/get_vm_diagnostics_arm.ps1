aeba7210cb36823d2f081cc931d0f8ea3dbe2e07
jiyongseong <jiyongseong@hotmail.com>
2016-09-26 07:45:51 +0900

    add arm

---
+++ powershell/get-VMDiagnosticsExtension/get_vm_diagnostics_arm.ps1
@@ -0,0 +1,12 @@
+﻿Login-AzureRmAccount
+
+$subscriptionName = "your subscription name"
+Select-AzureRmSubscription -SubscriptionName $subscriptionName
+
+$resourceGroupName = "your resource group name"
+$vmName = "your vm name"
+$configPath = 'C:\temp\filename_arm.xml'
+
+$extensionContext = Get-AzureRmVMDiagnosticsExtension -ResourceGroupName $resourceGroupName -VMName $vmName 
+$publicConfiguration = $extensionContext.PublicSettings | ConvertFrom-Json
+[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($publicConfiguration.xmlcfg)) | Out-File -Encoding utf8 -FilePath $configPath
