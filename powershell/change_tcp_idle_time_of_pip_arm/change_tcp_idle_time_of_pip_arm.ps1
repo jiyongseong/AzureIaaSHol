c64993e06b7215966d65304c97a0a268058dbf2c
jiyongseong <jiyongseong@hotmail.com>
2016-09-27 22:22:07 +0900

    change_tcp_idle_time_of_pip_arm

---
+++ powershell/change_tcp_idle_time_of_pip_arm/change_tcp_idle_time_of_pip_arm.ps1
@@ -0,0 +1,12 @@
+﻿Login-AzureRmAccount
+
+$subscriptionName = "your subscription name"
+
+Select-AzureRmSubscription -SubscriptionName $subscriptionName
+
+$resourceGroupName = "your resource group name"
+$pipName = "public ip name"
+
+$pip = Get-AzureRmPublicIpAddress -ResourceGroupName $resourceGroupName -Name $pipName
+$pip.IdleTimeoutInMinutes = 10
+Set-AzureRmPublicIpAddress -PublicIpAddress $pip 
