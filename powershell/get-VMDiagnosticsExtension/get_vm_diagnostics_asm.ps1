8718fa69000d97849b6b52146726c7318281eef4
jiyongseong <jiyongseong@hotmail.com>
2016-09-27 10:18:10 +0900

    get vm diag asm

---
+++ powershell/get-VMDiagnosticsExtension/get_vm_diagnostics_asm.ps1
@@ -0,0 +1,15 @@
+﻿Add-AzureAccount
+
+$subscriptionName = "your subscription name"
+$serviceName = "your cloud service name"
+$vmName = "your vm name"
+ 
+$configPath = "C:\temp\filename_asm.xml"
+ 
+Select-AzureSubscription -SubscriptionName $subscriptionName
+
+$vmContext = Get-AzureVM -ServiceName $serviceName -Name $vmName 
+
+$extensionContext = Get-AzureVMDiagnosticsExtension -VM $vmContext
+$publicConfiguration = $extensionContext.PublicConfiguration | ConvertFrom-Json
+[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($publicConfiguration.xmlcfg)) | Out-File -Encoding utf8 -FilePath $configPath
